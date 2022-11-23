2ac38c6e8c721ad799b30fbd4de75f0358938293
yego <yegokipkemoi254@gmail.com>
2022-11-23 21:58:54 +0300

    commit tree 5ffdc4cad006af56f14698394a01ba75c0690969

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,9 @@
 class BirdsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
+   rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
+
   # GET /birds
   def index
     birds = Bird.all
@@ -9,7 +12,7 @@ class BirdsController < ApplicationController
 
   # POST /birds
   def create
-    bird = Bird.create(bird_params)
+    bird = Bird.create!(bird_params)
     render json: bird, status: :created
   end
 
@@ -22,7 +25,7 @@ class BirdsController < ApplicationController
   # PATCH /birds/:id
   def update
     bird = find_bird
-    bird.update(bird_params)
+    bird.update!(bird_params)
     render json: bird
   end
 
@@ -45,6 +48,10 @@ class BirdsController < ApplicationController
 
   def render_not_found_response
     render json: { error: "Bird not found" }, status: :not_found
+  end
+
+  def render_unprocessable_entity_response(invalid)
+    render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity  
   end
 
 end
